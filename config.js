a74c1ce1aad33c0b482e2873040fb60dca04e06e
jamesy-tech <jamesycreator@gmail.com>
2025-04-18 09:50:27 +1200

    Update config.js
    - Changed Question 9 Option C to "SSR"
    - Changed Question 9's answer to "SSR" (Option C)

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ var questions = [
     { text: "What is Ari's favourite movie?", options: ["Madagascar", "Ice Age", "Lion King"], correctAnswer: "Madagascar", correctAnswerLetter: "A" },
     { text: "What is Ari's favourite song?", options: ["Money", "Miku", "New Zealand National Anthem"], correctAnswer: "Miku", correctAnswerLetter: "B" },
     { text: "What is Ari's favourite takeaway food?", options: ["KFC", "McDonald's", "Pizza Hut"], correctAnswer: "KFC", correctAnswerLetter: "A" },
-    { text: "What is Ari's favourite subject at school?", options: ["Maths", "Writing", "Reading"], correctAnswer: "Reading", correctAnswerLetter: "C" },
+    { text: "What is Ari's favourite subject at school?", options: ["Maths", "Writing", "SSR"], correctAnswer: "SSR", correctAnswerLetter: "C" },
     { text: "What is Ari's dad's first name?", options: ["Jeremiah", "Geoffrey", "Richard"], correctAnswer: "Richard", correctAnswerLetter: "C" },
     { text: "How many months old is Ari?", options: ["68", "88", "108"], correctAnswer: "108", correctAnswerLetter: "C" },
     { text: "What is Ari's favourite activity?", options: ["Watch TV", "Play video games", "Reading"], correctAnswer: "Watch TV", correctAnswerLetter: "A" },
