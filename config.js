8f6dd22800e8aa1e27cb0ca8ef8921848af52a8d
Jamesy-tech <jamesycreator@gmail.com>
2025-04-18 08:56:41 +1200

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+var quiz_name = "Ari's Birthday Quiz";
+var quiz_image_url = "images/shapes.png";
+var quiz_end_image_url = "images/stacked_shapes.png";
+var quiz_description = "Look up at the screen for the questions and write the letter for the correct answer on your sheet.";
+var quiz_end_description = "Made with Jamesy Creator";
+var quiz_author = "James Trendall";
+
+var questions = [
+    { text: "How old is Ari?", options: ["8", "9", "10"], correctAnswer: "9", correctAnswerLetter: "B" },
+    { text: "What is Ari's second favourite animal?", options: ["Aardvark", "Bear", "Cat"], correctAnswer: "Cat", correctAnswerLetter: "C" },
+    { text: "Who is Ari's favourite sports person? Clue - Football/Japanese", options: ["Tommy Smith", "Hiroki Sakai", "Neyder Moreno"], correctAnswer: "Hiroki Sakai", correctAnswerLetter: "C" },
+    { text: "What is Ari's second/middle name?", options: ["Bartholomew", "Ivanov", "Richard"], correctAnswer: "Richard", correctAnswerLetter: "C" },
+    { text: "What is Ari's favourite TV program?", options: ["Young Sheldon", "Peppa Pig", "Malcolm in the Middle"], correctAnswer: "Young Sheldon", correctAnswerLetter: "A" },
+    { text: "What is Ari's favourite movie?", options: ["Madagascar", "Ice Age", "Lion King"], correctAnswer: "Madagascar", correctAnswerLetter: "A" },
+    { text: "What is Ari's favourite song?", options: ["Money", "Miku", "New Zealand National Anthem"], correctAnswer: "Miku", correctAnswerLetter: "B" },
+    { text: "What is Ari's favourite takeaway food?", options: ["KFC", "McDonald's", "Pizza Hut"], correctAnswer: "KFC", correctAnswerLetter: "A" },
+    { text: "What is Ari's favourite subject at school?", options: ["Maths", "Writing", "Reading"], correctAnswer: "Reading", correctAnswerLetter: "C" },
+    { text: "What is Ari's dad's first name?", options: ["Jeremiah", "Geoffrey", "Richard"], correctAnswer: "Richard", correctAnswerLetter: "C" },
+    { text: "How many months old is Ari?", options: ["68", "88", "108"], correctAnswer: "108", correctAnswerLetter: "C" },
+    { text: "What is Ari's favourite activity?", options: ["Watch TV", "Play video games", "Reading"], correctAnswer: "Watch TV", correctAnswerLetter: "A" },
+     
+];
+
